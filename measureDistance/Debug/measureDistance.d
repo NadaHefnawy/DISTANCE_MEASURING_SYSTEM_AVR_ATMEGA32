e141fdb79f46f0e9f85861e72b91e2d1e8b66714
Nada Hefnawy <nadaelhefnawy2002@gmail.com>
2023-09-26 17:39:06 +0300

    measureDistance

---
+++ measureDistance/Debug/measureDistance.d
@@ -0,0 +1,12 @@
+measureDistance.o measureDistance.o: ../measureDistance.c ../lcd.h \
+  ../std_types.h ../ultrasonic.h ../common_macros.h ../gpio.h
+
+../lcd.h:
+
+../std_types.h:
+
+../ultrasonic.h:
+
+../common_macros.h:
+
+../gpio.h:
